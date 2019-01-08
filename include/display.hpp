48dd28033232e72d24f9e0523ae90607e712d41f
Delay <cpieri@student.42.fr>
2019-01-08 11:35:31 +0100

    display in progress

--- include/display.hpp
+++ include/display.hpp
@@ -6,7 +6,7 @@
 /*   By: delay <cpieri@student.42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/12 15:11:02 by delay             #+#    #+#             */
-/*   Updated: 2019/01/03 13:50:36 by delay            ###   ########.fr       */
+/*   Updated: 2019/01/08 11:32:38 by delay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,25 @@
 # include <SDL.h>
 # include "vector.hpp"
 # include "color.hpp"
-# define WIN_CENTER SDL_WINDOWPOS_CENTERED
+
+# define	WIN_CENTER SDL_WINDOWPOS_CENTERED
+# define	P(x)		(x * 10)
+# define	WHITE		0xFFFFFF
+# define	GRAY		0xAD9A84
+# define	DARKGRAY	0x524D42
+# define	BLACK		0x000000
+# define	PURPLE		0x940042
+# define	PINK		0xCE4584
+# define	RED			0xDE1C21
+# define	ORANGE		0xFFAA10
+# define	BROWN		0xCE8E42
+# define	BEIGE		0xFFD794
+# define	YELLOW		0xF7E700
+# define	LIGHTGREEN	0x84CE42
+# define	GREEN		0x008A52
+# define	DARKBLUE	0x004184
+# define	BLUE		0x4286CE
+# define	LIGHTBLUE	0x7BBAFF
 
 class	Window
 {
@@ -38,12 +56,16 @@ class	Window
 		void			create(char const * title, int w, int h);
 		void			destroy(void);
 		void			change_size(void);
+		int				height(void) const;
+		int				width(void) const;
 		Vector4d		get_size(void);
 		SDL_Window*		get(void);
 
 		/*
 		**	Fonctions for Renders
 		*/
+		void			fill(int color = 0x000000);
+		void			fill(Color c);
 		void			clear(int color = 0x000000);
 		void			clear(Color c);
 		void			present(void) const;
@@ -54,9 +76,17 @@ class	Window
 		*/
 		int				drawPixel(int x, int y, int color = 0xffffff);
 		int				drawPixel(int x, int y, Color c);
+		int				drawLine(int x0, int y0, int x1, int y1);
+		void			drawFastVLine(int x, int y, int h, int color = 0xffffff);
+		void			drawFastHLine(int x, int y, int w, int color = 0xffffff);
 		int				drawRect(Vector4d rect, int color = 0xffffff);
 		int				drawRect(Vector4d rect, Color c);
 		int				drawRect(int x, int y, int w, int h);
+		int				fillRect(Vector4d rect, int color = 0xffffff);
+		int				fillRect(Vector4d rect, Color c);
+		int				fillRect(int x, int y, int w, int h);
+		void			drawCircle(int x, int y, int r, int color = 0xffffff);
+		//void			fillCircle(int x, int y, int r, int color = 0xffffff);
 		void			setColor(Color c);
 		void			setColor(int color);
 		void			setColor(int r, int g, int b);
--- srcs/display.cpp
+++ srcs/display.cpp
@@ -6,13 +6,14 @@
 /*   By: delay <cpieri@student.42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/12 15:44:57 by delay             #+#    #+#             */
-/*   Updated: 2019/01/03 14:02:52 by delay            ###   ########.fr       */
+/*   Updated: 2019/01/08 11:31:33 by delay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "display.hpp"
 #include "color.hpp"
 #include <iostream>
+#include <stdlib.h>
 
 Window::Window(void) : _loop(1)
 {
@@ -49,6 +50,16 @@ void			Window::change_size(void)
 	std::cout << "change_size " << this->_width << " " << this->_height << std::endl;
 }
 
+int				Window::height(void) const
+{
+	return (this->_height);
+}
+
+int				Window::width(void) const
+{
+	return (this->_width);
+}
+
 Vector4d		Window::get_size(void)
 {
 	return (Vector4d(0, 0, this->_width, this->_height));
@@ -62,6 +73,25 @@ SDL_Window*		Window::get(void)
 /*******************************************************************************
 **							Fonctions for Renders							  **
 *******************************************************************************/
+
+void			Window::fill(int color)
+{
+	this->_color.int_to_color(color);
+	SDL_SetRenderDrawColor(this->_rend, 
+			this->_color.get_red(), this->_color.get_green(),
+			this->_color.get_blue(), 1);
+	SDL_RenderClear(this->_rend);
+}
+
+void			Window::fill(Color c)
+{
+	this->_color = c;
+	SDL_SetRenderDrawColor(this->_rend, 
+			this->_color.get_red(), this->_color.get_green(),
+			this->_color.get_blue(), 1);
+	SDL_RenderClear(this->_rend);
+}
+
 
 void			Window::clear(int color)
 {
@@ -102,7 +132,7 @@ int				Window::drawPixel(int x, int y, int color)
 	this->_color.int_to_color(color);
 	SDL_SetRenderDrawColor(this->_rend, this->_color.get_red(),
 			this->_color.get_green(), this->_color.get_blue(), 1);
-	ret = SDL_RenderDrawPoint(this->_rend, x, y);
+	ret = SDL_RenderDrawPoint(this->_rend, P(x), P(y));
 	return (ret);
 }
 
@@ -113,9 +143,49 @@ int				Window::drawPixel(int x, int y, Color c)
 	this->_color = c;
 	SDL_SetRenderDrawColor(this->_rend, this->_color.get_red(),
 			this->_color.get_green(), this->_color.get_blue(), 1);
-	ret = SDL_RenderDrawPoint(this->_rend, x, y);
+	ret = SDL_RenderDrawPoint(this->_rend, P(x), P(y));
 	return (ret);
 
+}
+
+int				Window::drawLine(int x0, int y0, int x1, int y1)
+{
+	int		ret;
+
+	SDL_SetRenderDrawColor(this->_rend, this->_color.get_red(),
+			this->_color.get_green(), this->_color.get_blue(), 1);
+	ret = SDL_RenderDrawLine(this->_rend, P(x0), P(y0), P(x1), P(y1));
+	return (ret);
+}
+
+void			Window::drawFastVLine(int x, int y, int h, int color)
+{
+	x = P(x);
+	y = P(y);
+	h = P(h);
+	this->_color.int_to_color(color);
+	SDL_SetRenderDrawColor(this->_rend, this->_color.get_red(),
+			this->_color.get_green(), this->_color.get_blue(), 1);
+	if (h < 0) {
+		y += h + 1;
+		h = abs(h);
+	}
+	SDL_RenderDrawLine(this->_rend, x, y, x, y + h - 1);
+}
+
+void			Window::drawFastHLine(int x, int y, int w, int color)
+{
+	x = P(x);
+	y = P(y);
+	w = P(w);
+	this->_color.int_to_color(color);
+	SDL_SetRenderDrawColor(this->_rend, this->_color.get_red(),
+			this->_color.get_green(), this->_color.get_blue(), 1);
+	if (w < 0) {
+		x += w + 1;
+		w = abs(w);
+	}
+	SDL_RenderDrawLine(this->_rend, x, y, x + w - 1, y);
 }
 
 int				Window::drawRect(Vector4d rect, int color)
@@ -124,13 +194,13 @@ int				Window::drawRect(Vector4d rect, int color)
 	int 		ret;
 
 	this->_color.int_to_color(color);
-	sdl_rect.x = rect.get_x(); 
-	sdl_rect.y = rect.get_y(); 
-	sdl_rect.w = rect.get_z(); 
-	sdl_rect.h = rect.get_w();
+	sdl_rect.x = P(rect.get_x());
+	sdl_rect.y = P(rect.get_y());
+	sdl_rect.w = P(rect.get_z());
+	sdl_rect.h = P(rect.get_w());
 	SDL_SetRenderDrawColor(this->_rend, this->_color.get_red(),
 			this->_color.get_green(), this->_color.get_blue(), 1);
-	ret = SDL_RenderFillRect(this->_rend, &sdl_rect);
+	ret = SDL_RenderDrawRect(this->_rend, &sdl_rect);
 	return (ret);
 }
 
@@ -140,13 +210,13 @@ int				Window::drawRect(Vector4d rect, Color c)
 	int 		ret;
 
 	this->_color = c;
-	sdl_rect.x = rect.get_x(); 
-	sdl_rect.y = rect.get_y(); 
-	sdl_rect.w = rect.get_z(); 
-	sdl_rect.h = rect.get_w();
+	sdl_rect.x = P(rect.get_x());
+	sdl_rect.y = P(rect.get_y());
+	sdl_rect.w = P(rect.get_z());
+	sdl_rect.h = P(rect.get_w());
 	SDL_SetRenderDrawColor(this->_rend,  this->_color.get_red(),
 			this->_color.get_green(), this->_color.get_blue(), 1);
-	ret = SDL_RenderFillRect(this->_rend, &sdl_rect);
+	ret = SDL_RenderDrawRect(this->_rend, &sdl_rect);
 	return (ret);
 }
 
@@ -155,15 +225,103 @@ int				Window::drawRect(int x, int y, int w, int h)
 	SDL_Rect	sdl_rect;
 	int 		ret;
 
-	sdl_rect.x = x; 
-	sdl_rect.y = y; 
-	sdl_rect.w = w; 
-	sdl_rect.h = h;
+	sdl_rect.x = P(x);
+	sdl_rect.y = P(y);
+	sdl_rect.w = P(w);
+	sdl_rect.h = P(h);
+	SDL_SetRenderDrawColor(this->_rend, this->_color.get_red(),
+			this->_color.get_green(), this->_color.get_blue(), 1);
+	ret = SDL_RenderDrawRect(this->_rend, &sdl_rect);
+	return (ret);
+}
+
+
+int				Window::fillRect(Vector4d rect, int color)
+{
+	SDL_Rect	sdl_rect;
+	int 		ret;
+
+	this->_color.int_to_color(color);
+	sdl_rect.x = P(rect.get_x()); 
+	sdl_rect.y = P(rect.get_y());
+	sdl_rect.w = P(rect.get_z()); 
+	sdl_rect.h = P(rect.get_w());
 	SDL_SetRenderDrawColor(this->_rend, this->_color.get_red(),
 			this->_color.get_green(), this->_color.get_blue(), 1);
 	ret = SDL_RenderFillRect(this->_rend, &sdl_rect);
 	return (ret);
 }
+
+int				Window::fillRect(Vector4d rect, Color c)
+{
+	SDL_Rect	sdl_rect;
+	int 		ret;
+
+	this->_color = c;
+	sdl_rect.x = P(rect.get_x()); 
+	sdl_rect.y = P(rect.get_y());
+	sdl_rect.w = P(rect.get_z()); 
+	sdl_rect.h = P(rect.get_w());
+	SDL_SetRenderDrawColor(this->_rend,  this->_color.get_red(),
+			this->_color.get_green(), this->_color.get_blue(), 1);
+	ret = SDL_RenderFillRect(this->_rend, &sdl_rect);
+	return (ret);
+}
+
+int				Window::fillRect(int x, int y, int w, int h)
+{
+	SDL_Rect	sdl_rect;
+	int 		ret;
+
+	sdl_rect.x = P(x); 
+	sdl_rect.y = P(y); 
+	sdl_rect.w = P(w); 
+	sdl_rect.h = P(h);
+	SDL_SetRenderDrawColor(this->_rend, this->_color.get_red(),
+			this->_color.get_green(), this->_color.get_blue(), 1);
+	ret = SDL_RenderFillRect(this->_rend, &sdl_rect);
+	return (ret);
+}
+
+void			Window::drawCircle(int x0, int y0, int r, int color)
+{
+	x0 = P(x0);
+	y0 = P(y0);
+	r = P(r);
+	int		f = 1 - r;
+	int		ddF_x = 1;
+	int		ddF_y = -2 * r;
+	int		x = 0;
+	int		y = r;
+
+	this->_color.int_to_color(color);
+	SDL_SetRenderDrawColor(this->_rend, this->_color.get_red(),
+			this->_color.get_green(), this->_color.get_blue(), 1);
+	SDL_RenderDrawPoint(this->_rend, x0, y0+r);
+	SDL_RenderDrawPoint(this->_rend, x0, y0-r);
+	SDL_RenderDrawPoint(this->_rend, x0+r, y0);
+	SDL_RenderDrawPoint(this->_rend, x0-r, y0);
+	while (x < y) {
+		if (f >= 0) {
+			y--;
+			ddF_y += 2;
+			f += ddF_y;
+		}
+		x++;
+		ddF_x += 2;
+		f += ddF_x;
+		SDL_RenderDrawPoint(this->_rend, x0 + x, y0 + y);
+		SDL_RenderDrawPoint(this->_rend, x0 - x, y0 + y);
+		SDL_RenderDrawPoint(this->_rend, x0 + x, y0 - y);
+		SDL_RenderDrawPoint(this->_rend, x0 - x, y0 - y);
+		SDL_RenderDrawPoint(this->_rend, x0 + y, y0 + x);
+		SDL_RenderDrawPoint(this->_rend, x0 - y, y0 + x);
+		SDL_RenderDrawPoint(this->_rend, x0 + y, y0 - x);
+		SDL_RenderDrawPoint(this->_rend, x0 - y, y0 - x);
+	}
+}
+
+
 
 void			Window::setColor(Color c)
 {
